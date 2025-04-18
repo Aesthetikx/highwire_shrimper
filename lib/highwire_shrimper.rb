43ff3d4e38ce6d2916f1113e8d07d1f21a9c2cb9
John DeSilva <desilvjo@umich.edu>
2025-04-18 15:59:39 -0400

    Add TUI entrypoint

--- lib/highwire_shrimper.rb
+++ lib/highwire_shrimper.rb
@@ -1,8 +1,11 @@
+require_relative 'highwire_shrimper/tui'
 require_relative 'highwire_shrimper/version'
 
 # HighwireShrimper is an IRC client and library.
 module HighwireShrimper
   class Error < StandardError; end
 
-  def self.shrimp!; end
+  def self.shrimp!
+    TUI.open!
+  end
 end
---
+++ lib/highwire_shrimper/tui.rb
@@ -0,0 +1,6 @@
+module HighwireShrimper
+  # The terminal user interface
+  module TUI
+    def self.open!; end
+  end
+end
