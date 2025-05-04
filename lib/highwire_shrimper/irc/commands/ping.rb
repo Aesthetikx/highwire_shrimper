f6e222ba91d5bf8926742c056acd6b6825342fcf
John DeSilva <desilvjo@umich.edu>
2025-05-03 21:16:54 -0400

    Implement PING command

---
+++ lib/highwire_shrimper/irc/commands/ping.rb
@@ -0,0 +1,23 @@
+module HighwireShrimper
+  module IRC
+    module Commands
+      # https://datatracker.ietf.org/doc/html/rfc1459#section-4.6.2
+      class Ping
+        attr_reader :server1, :server2
+
+        def initialize(server1:, server2: nil)
+          @server1 = ServerName server1
+          @server2 = ServerName server2
+        end
+
+        def to_s
+          if server2
+            "PING #{server1} #{server2}"
+          else
+            "PING :#{server1}"
+          end
+        end
+      end
+    end
+  end
+end
