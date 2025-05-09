d2e22f041682e7e5d8c48a73445b71765dc3b9e0
John DeSilva <desilvjo@umich.edu>
2025-05-08 20:23:06 -0400

    Add ssl argument to Server

--- lib/highwire_shrimper/irc/server.rb
+++ lib/highwire_shrimper/irc/server.rb
@@ -4,11 +4,14 @@ module HighwireShrimper
     class Server
       attr_reader :name, :hostname, :port
 
-      def initialize(name:, hostname:, port:)
+      def initialize(name:, hostname:, port:, ssl:)
         @name = name
         @hostname = ServerName(hostname)
         @port = port
+        @ssl = ssl
       end
+
+      def ssl? = @ssl
     end
   end
 end
