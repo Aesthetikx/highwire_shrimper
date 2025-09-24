045297fb3b420b4c5ba27bc1aae2281f1eb6547c
John DeSilva <desilvjo@umich.edu>
2025-09-23 21:49:31 -0400

    Fix linter warning

--- lib/highwire_shrimper/irc/server_name.rb
+++ lib/highwire_shrimper/irc/server_name.rb
@@ -17,13 +17,13 @@ module HighwireShrimper
 
       RFC_1123_REGEX = /
         \A
-        (?=.{1,255}\z)                  # The entire length must be between 1 and 255 characters
-        (?:                             # A non-capturing group for repeated labels
-          [a-z0-9]                      # A label must start with alphanum
-          (?:[a-z0-9\-]{0,61}[a-z0-9])? # The middle and end of label (optional for 1-character labels)
-          \.                            # A label ends with a period
-        )+                              # There must be at least one label before the TLD
-        [a-z]{2,63}                     # The TLD must be letters only between 2–63 characters
+        (?=.{1,255}\z)                 # The entire length must be between 1 and 255 characters
+        (?:                            # A non-capturing group for repeated labels
+          [a-z0-9]                     # A label must start with alphanum
+          (?:[a-z0-9-]{0,61}[a-z0-9])? # The middle and end of label (optional for 1-character labels)
+          \.                           # A label ends with a period
+        )+                             # There must be at least one label before the TLD
+        [a-z]{2,63}                    # The TLD must be letters only between 2–63 characters
         \z
       /ix # Case insensitive, extended mode
 
